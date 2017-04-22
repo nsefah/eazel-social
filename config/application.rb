26982e2aafbc255ffbd2888b20db999e50a23b16
nsefah <nksefah@colby.edu>
2017-04-22 10:32:03 -0400

    Starting Avatar creation, usercontroller up next

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module EazelSocial
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Once the schema is set up, the next step is to get the models set up
+    # and the proper inheritance tree established. But, before you do that,
+    #make sure to add any subfolders in your app/models folder to the autoload
+    #path in the application.rb file. This will ensure that Rails knows to look
+    #at subfolders, which may be good to use to effectively organize your model
+    #files.
+
+    config.autoload_paths += %W(#{config.root}/app/models/user)
   end
 end
